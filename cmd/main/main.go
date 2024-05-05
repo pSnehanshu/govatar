47001b035cf9ce7bea0dc96b1c9d3505416773c8
Snehanshu <hello@snehanshu.com>
2024-05-05 19:36:00 +0530

    use .env file for config

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/pSnehanshu/govatar/ent"
 	"github.com/pSnehanshu/govatar/internals/app"
 )
 
 func main() {
+	// Initializing .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading .env file: %v", err)
+	}
+
 	// Initialize database connection
-	db, err := ent.Open("postgres", "postgresql://postgres:password@127.0.0.1/govatar?sslmode=disable")
+	db, err := ent.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
 	defer db.Close()
 
+	// Determine the port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "7180"
+	}
+	listenAddr := fmt.Sprintf(":%s", port)
+
 	// Initialize and start the server (app)
-	err = app.NewApp(db).Listen(":3000")
+	err = app.NewApp(db).Listen(listenAddr)
 	log.Fatalf("Server ended with error: %v", err)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	entgo.io/ent v0.13.1
 	github.com/gofiber/fiber/v2 v2.52.4
 	github.com/google/uuid v1.5.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
 
