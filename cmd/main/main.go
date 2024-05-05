89e86b4daf5e112f786ed06fedea2152a1bb6c65
Snehanshu <hello@snehanshu.com>
2024-05-05 19:18:17 +0530

    remove database package, use Ent directly

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,15 +4,19 @@ import (
 	"log"
 
 	_ "github.com/lib/pq"
+	"github.com/pSnehanshu/govatar/ent"
 	"github.com/pSnehanshu/govatar/internals/app"
-	"github.com/pSnehanshu/govatar/internals/database"
 )
 
 func main() {
-	db := database.GetClient()
+	// Initialize database connection
+	db, err := ent.Open("postgres", "postgresql://postgres:password@127.0.0.1/govatar?sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
 	defer db.Close()
 
-	app := app.NewApp(db)
-
-	log.Fatalf("Server ended with error: %v", app.Listen(":3000"))
+	// Initialize and start the server (app)
+	err = app.NewApp(db).Listen(":3000")
+	log.Fatalf("Server ended with error: %v", err)
 }
